5b03f58795c5207cbf51b174a900e06e92add8cb
diep1cp <cdiep@usf.edu>
2025-02-26 11:58:08 -0500

    Added the code

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello world class demo!");
+            Console.ReadLine();
+
+
+            int x;
+            x = 3;
+
+            Debug.WriteLine("Hello, Chi!");
+            Debug.WriteLine("Hello, Git!");
+            Debug.WriteLine("Hello, Chi!");
+            Debug.WriteLine("Hello, Git!");
+        }
+    }
+}
